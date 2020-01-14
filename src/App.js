f9544faeb9d40281df5d19dceb5cd7a6a53633ce
Jessica <jess.appelbaum@gmail.com>
2020-01-13 16:00:10 -0800

    set initial state

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
-import React, {Component, useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
-const classes_url = 'https://api.open5e.com/classes/';
-const races_url = 'https://api.open5e.com/races/';
+const classesUrl = 'https://api.open5e.com/classes/';
+const racesUrl = 'https://api.open5e.com/races/';
 
 function App() {
     const [currentClass, setClass] = useState("");
     const [currentRace, setRace] = useState("");
 
+    const fetchClass = async () => {
+        fetch(classesUrl)
+        .then(res => res.json())
+        .then(response => {
+            const all_classes = response.results;
+            const random_item = all_classes[Math.floor(Math.random()*all_classes.length)];
+            setClass(random_item.name);
+        })
+        .catch(error => console.log(error));
+    };
+
+    const fetchRace = async () => {
+        fetch(racesUrl)
+        .then(res => res.json())
+        .then(response => {
+            const all_races = response.results;
+            const random_item = all_races[Math.floor(Math.random()*all_races.length)];
+            setRace(random_item.name);
+        })
+        .catch(error => console.log(error));
+    };
+
+    useEffect(() => {
+        fetchClass();
+      }, []);
+
+    useEffect(() => {
+        fetchRace();
+      }, []);
+
+
     return (
         <div>
             <h1>Your class is: {currentClass}</h1>
             <button
-                onClick={
-                    () => {
-                    fetch(classes_url)
-                        .then(res => res.json())
-                        .then(response => {
-                            const all_classes = response.results;
-                            const random_item = all_classes[Math.floor(Math.random()*all_classes.length)];
-                            setClass(random_item.name);
-                        })
-                        .catch(error => console.log(error));
-                }
-                }>Get another class</button>
+                onClick={ () => fetchClass() }
+            >Get another class</button>
 
             <h1>Your race is: {currentRace}</h1>
             <button
-                onClick={
-                    () => {
-                    fetch(races_url)
-                        .then(res => res.json())
-                        .then(response => {
-                            const allRaces = response.results;
-                            const randomRace = allRaces[Math.floor(Math.random()*allRaces.length)];
-                            setRace(randomRace.name);
-                        })
-                        .catch(error => console.log(error));
-                }
-                }>Get another race</button>
+                onClick={ () => fetchRace()}
+            >Get another race</button>
         </div>
     )
 }
